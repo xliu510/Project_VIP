1c063883807bd5eb64c63e6e13355475a4483f54
Berga <42157734+berga23@users.noreply.github.com>
2019-01-26 12:31:07 -0800

    pushing flask app and scrape formulas

--- Flask/app.py
+++ Flask/app.py
@@ -1,60 +1,38 @@
-import dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_crypto
 
-import flask
-import pandas as pd
-import time
-import os
+# Create an instance of Flask
+app = Flask(__name__)
 
-server = flask.Flask('app')
-server.secret_key = os.environ.get('secret_key', 'secret')
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
 
-app = dash.Dash('app', server=server)
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
 
-app.scripts.config.serve_locally = False
-dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+    # Find one record of data from the mongo database
+    full_data = mongo.db.collection.find_one()
 
-app.layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'},
-            {'label': 'Coke', 'value': 'COKE'}
-        ],
-        value='TSLA'
-    ),
-    dcc.Graph(id='my-graph')
-], className="container")
+    # Return template and data
+    return render_template("index.html", crypto=full_data)
 
-@app.callback(Output('my-graph', 'figure'),
-              [Input('my-dropdown', 'value')])
 
-def update_graph(selected_dropdown_value):
-    dff = df[df['Stock'] == selected_dropdown_value]
-    return {
-        'data': [{
-            'x': dff.Date,
-            'y': dff.Close,
-            'line': {
-                'width': 3,
-                'shape': 'spline'
-            }
-        }],
-        'layout': {
-            'margin': {
-                'l': 30,
-                'r': 20,
-                'b': 30,
-                't': 20
-            }
-        }
-    }
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
 
-if __name__ == '__main__':
-    app.run_server()
+    # Run the scrape function
+    crypto_data = scrape_crypto.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, crypto_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
