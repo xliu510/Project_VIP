ab2a9e87c4a3d1ad2c32dc625c74dd2db8631f56
itsme291 <42480508+itsme291@users.noreply.github.com>
2019-01-29 19:19:19 -0800

    Fixing Heatmap Dark Map zoom issue

--- Leaflet/static/js/logic.js
+++ Leaflet/static/js/logic.js
@@ -11,7 +11,7 @@ function createMap(atmlocations, heat) {
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-    maxZoom: 18,
+    maxZoom: 10,
     id: "mapbox.dark",
     accessToken: API_KEY
   });
@@ -68,13 +68,13 @@ d3.json("atm-data.html", function(response) {
       latlngs, 
       {radius: 50}, 
       {maxZoom:3}, 
-      {minOpacity:0},
-      {gradient: {
-        0.0: 'green',
-        0.45: 'yellow',
-        0.9: 'red'
-      }  
-      }
+      {minOpacity:0} //,
+      // {gradient: {
+      //   0.0: 'green',
+      //   0.45: 'yellow',
+      //   0.9: 'red'
+      // }  
+      // }
     );
 
   // Add our marker cluster layer to the map
